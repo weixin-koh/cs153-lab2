71feae47b96a656610eea9d4a1e7f23f49406d70
Koh Wei Xin <e0201350@u.nus.edu>
2020-03-12 16:44:02 -0700

    Add implementation of shm_open and shm_close, and add .gitignore

--- shm.c
+++ shm.c
@@ -29,21 +29,70 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
+  int i;
 
-//you write this
+  acquire(&(shm_table.lock));
 
+  for (i = 0; i< 64; i++) {
+    if (shm_table.shm_pages[i].id == id) {
+      mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      shm_table.shm_pages[i].refcnt++;
+      *pointer=(char *) PGROUNDUP(myproc()->sz);
+      myproc()->sz += PGSIZE;
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
 
+  // Shared memory DNE
+  for (i = 0; i < 64; ++i) {
+    if (shm_table.shm_pages[i].id == 0) {
+      shm_table.shm_pages[i].id = id;
+      shm_table.shm_pages[i].frame = kalloc();
+      shm_table.shm_pages[i].refcnt = 1;
+      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+      mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      *pointer=(char *) PGROUNDUP(myproc()->sz);
+      myproc()->sz += PGSIZE;
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
+  
+  release(&(shm_table.lock));
 
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0;
 }
 
 
 int shm_close(int id) {
-//you write this too!
+  int i;
 
+  initlock(&(shm_table.lock), "SHM lock");
+  acquire(&(shm_table.lock));
 
+  for (i = 0; i< 64; i++) {
+    if (shm_table.shm_pages[i].id == id) {
+      shm_table.shm_pages[i].refcnt--;
 
+      if (shm_table.shm_pages[i].refcnt > 0) {
+        break;
+      }
 
-return 0; //added to remove compiler warning -- you should decide what to return
+      shm_table.shm_pages[i].id = 0;
+      shm_table.shm_pages[i].frame = 0;
+      shm_table.shm_pages[i].refcnt = 0;
+      break;
+    }
+  }
+
+  // Shared memory item not found
+  if (shm_table.shm_pages[i].id != id) {
+    release(&(shm_table.lock));
+    return 1;
+  }
+
+  release(&(shm_table.lock));
+
+return 0;
 }
