ff03c99ee4ee9feed570e314185ff8edf0365850
Koh Wei Xin <e0201350@u.nus.edu>
2020-03-13 00:17:14 -0700

    Reinstate Lab 3

--- shm.c
+++ shm.c
@@ -29,71 +29,21 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-  int i;
 
-  acquire(&(shm_table.lock));
+//you write this
 
-  // Case 1: Memory segment already exists
-  for (i = 0; i < 64; i++) {
-    if (shm_table.shm_pages[i].id == id) {
-      mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
-      shm_table.shm_pages[i].refcnt++;
-      *pointer = (char *) PGROUNDUP(myproc()->sz);
-      myproc()->sz += PGSIZE;
-      release(&(shm_table.lock));
-      return 0;
-    }
-  }
 
-  // Case 2: Shared memory segment does not exist
-  for (i = 0; i < 64; i++) {
-    if (shm_table.shm_pages[i].id == 0) {
-      shm_table.shm_pages[i].id = id;
-      shm_table.shm_pages[i].frame = kalloc();
-      shm_table.shm_pages[i].refcnt = 1;
-      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
-      mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
-      *pointer = (char *) PGROUNDUP(myproc()->sz);
-      myproc()->sz += PGSIZE;
-      release(&(shm_table.lock));
-      return 0;
-    }
-  }
 
-  release(&(shm_table.lock));
 
-  return 0;
+return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-  int i;
+//you write this too!
 
-  initlock(&(shm_table.lock), "SHM lock");
-  acquire(&(shm_table.lock));
 
-  for (i = 0; i< 64; i++) {
-    if (shm_table.shm_pages[i].id == id) {
-      shm_table.shm_pages[i].refcnt--;
 
-      if (shm_table.shm_pages[i].refcnt > 0) {
-        break;
-      }
 
-      shm_table.shm_pages[i].id = 0;
-      shm_table.shm_pages[i].frame = 0;
-      shm_table.shm_pages[i].refcnt = 0;
-      break;
-    }
-  }
-
-  // Shared memory item not found
-  if (shm_table.shm_pages[i].id != id) {
-    release(&(shm_table.lock));
-    return 1;
-  }
-
-  release(&(shm_table.lock));
-
-return 0;
+return 0; //added to remove compiler warning -- you should decide what to return
 }
